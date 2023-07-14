70249e4b6c0695ae18dea8091f85d123968f962f
igor.vorontsov <igor.vorontsov@extrawest.com>
2023-07-14 18:47:10 +0300

    added api property to change-password dto

--- src/auth/dtos/change-password.dto.ts
+++ src/auth/dtos/change-password.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, IsNotEmpty } from "class-validator";
 
 export class ChangePasswordDto {
+
+    @ApiProperty({example: "somepassword"})
     @IsString()
     @IsNotEmpty()
 	readonly password: string;
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
 			.setTitle("Test app")
 			.setDescription("Tesr Rest API description")
 			.setVersion("1.0")
-			.addTag("test")
 			.build();
 		const document = SwaggerModule.createDocument(
 			app,
